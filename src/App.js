d94e43c792d4b60a886bdcdf78e2fb9b7863e136
dimusiq <dumojke@gmail.com>
2022-04-18 14:48:46 +0500

    added react-router, routes, pages  and navbar components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar.component';
+import Offers from './pages/Offers.page';
+import Explore from './pages/Explore.page';
+import Profile from './pages/Profile.page';
+import ForgotPassword from './pages/Forgot-password.page';
+import SignUp from './pages/SignUp.page';
+import SignIn from './pages/SignIn.page';
+
+
 
 function App() {
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar/>
+      </Router>
     </>
   );
 }
